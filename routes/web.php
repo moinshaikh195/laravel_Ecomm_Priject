64caab76435b2d10c28fc2691fec36f96515b248
moinshaikh195 <shaikhmoin.abdul195@gmail.com>
2021-01-14 13:22:26 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::view('/','login');
+Route::view('/login','login');
+Route::post('/login',[UserController::class,'login']);
+Route::post('/',[ProductController::class,'index']);
